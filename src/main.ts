b069bd972c836c9af18bb1d714296de5a11f61d3
Kamalen <tidiusff@gmail.com>
2024-12-23 10:41:40 +0100

    Integration projet Angular

--- src/main.ts
+++ src/main.ts
@@ -8,9 +8,6 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
 
-
-
-// WEBPACK FOOTER //
-// ./src/main.ts
